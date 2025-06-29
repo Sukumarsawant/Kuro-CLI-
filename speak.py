4cacf4a568e36fc8c075beb9cacccf22a36f2a0f
Sukumarsawant <sawantsukumar@gmail.com>
2025-06-30 02:40:07 +0530

    Initial and first commit

---
+++ speak.py
@@ -0,0 +1,17 @@
+# speak.py
+import pyttsx3
+
+engine = pyttsx3.init()
+engine.setProperty('rate', 150)  # Speed of speech
+
+
+voices = engine.getProperty('voices')
+for voice in voices:
+    if 'female' in voice.name.lower():
+        engine.setProperty('voice', voice.id)
+        break
+
+def speak(text):
+    print(text)
+    engine.say(text)
+    engine.runAndWait()
