db9ed41f6c650124f7bc7a71e3e035dcf6bfa8cb
Sukumarsawant <your_email@example.com>
2025-07-03 18:54:31 +0530

    added GUI,added voices, fixed opening issues, removed write_code, updated prompt

--- gemini_ai.py
+++ gemini_ai.py
@@ -5,18 +5,20 @@ import os
 load_dotenv()
 genai.configure(api_key = os.getenv("GEMINI_API_KEY"))
 
-def get_command_from_gemini(user_input):
+def get_code_from_gemini(user_input):
     model = genai.GenerativeModel("gemini-2.0-flash-lite")
 
     # WIll be updated later to avoid jailbreak - sukumar
     prompt = f"""
 You are a terminal assistant.
-Convert the following natural language instruction into a valid terminal command.
+Convert the following natural language instruction into a valid "WINDOWS" terminal command.
 ONLY return the command (no explanations, no repetition, no markdown).
 
 User: {user_input}
 Shell:
-
+ 
+if user is telling to write code in a file then reply in the following format
+file_name lang_name which_code_u_want_to_write
 """
     # print("Gemini raw output:", command)
 
